05507ea35675393b36d5a4fba1f3f2077f7eb243
denali990 <jqrid10@gmail.com>
2026-04-01 17:14:04 -0700

    Create achievements.js file with ACHIEVEMENTS array

---
+++ achievements.js
@@ -0,0 +1,16 @@
+const ACHIEVEMENTS = [
+    {
+        name: 'First Cookie',
+        condition: 'Click the cookie for the first time'
+    },
+    {
+        name: 'Cookie Master',
+        condition: 'Click the cookie 100 times'
+    },
+    {
+        name: 'Cookie Overload',
+        condition: 'Click the cookie 1000 times'
+    }
+];
+
+module.exports = ACHIEVEMENTS;
